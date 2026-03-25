abb8aebcbc5119392b6bbe454b7ad4695536f97e
Salome Vizcarra <s.vizcarramartinez@dxc.com>
2026-03-25 00:58:51 -0700

    Initial commit

---
+++ Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Salome Vizcarra on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
